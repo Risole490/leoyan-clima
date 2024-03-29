bc9d1b4b1ccdd17919a7b420917fa78d4a77fdf2
Leoyan <71907948+Risole490@users.noreply.github.com>
2024-03-29 18:14:07 -0300

    Seção de infos da cidade atualizada

--- Scripts/testapi.js
+++ Scripts/testapi.js
@@ -1,15 +1,16 @@
 const apiKEY = '563d3432cd1394d7ff15b978d9aa148e'
-const apiCountryURL = 'https://flagcdn.com/16x12/ua.png'
 const timeKEY = '174bbe7276ed4359a11f5024026858aa'
 
 const cityInput = document.querySelector('#city-input')
 const searchBtn = document.querySelector('#search')
 
-const cityInfo = document.querySelector('#city-infos')
+const cityInfos = document.querySelector('#city-infos-container')
 
 
 //Functions
-function getWeatherData(city){
+async function getWeatherData(city){
+    let cidadeNome
+    let cidadePais
 
     if(!city){
         alert('Selecione uma cidade.')
@@ -17,66 +18,60 @@ function getWeatherData(city){
     }
 
     const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKEY}&lang=pt_br`
-//  const forecastURL = `https://pro.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKEY}`
 
-    fetch(currentWeatherURL)
-        .then(response => response.json())
-        .then(data => {
-            displayWeather(data)
-        })
-        .catch(error => {
+    try{ 
+        const response = await fetch(currentWeatherURL)
+        const data = await response.json()
+        cidadeNome = data.name
+        cidadePais = data.sys.country
+        }
+     catch(error) {
             console.error('Erro fetching current weather data:', error);
             alert('Error fetching current weather data. Please try again.')
-        })
+        }
+
+    const horarioCidadeFormatado = await timeLocal(cidadeNome)
+    
+    displayWeather(cidadeNome,cidadePais,horarioCidadeFormatado)
 }
 
-// function timeConverter(timestampUnix){
-//     var a = new Date(timestampUnix*1000)
-//     var months = ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'];
-//     var year = a.getFullYear();
-//     var month = months[a.getMonth()];
-//     var date = a.getDate();
-//     var hour = a.getHours();
-//     var min = a.getMinutes();
-//     var time = date + ' ' + month + ' ' + year + ' ' + hour + 'h' + min;
-//     return time;
-// }
+function converterData(dataCompleta){
+    const a = new Date(dataCompleta)
+    const months = ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate()
+    const hour = a.getHours()
+    const min = a.getMinutes()
+    const time = date + ' ' + month + ' ' + year + ' ' + hour + 'h' + min;
+    return time;
+}
 
-function timeLocal(latitude, longitude){
-    if(!latitude || !longitude){
-        alert('Sem lat e long.')
+async function timeLocal(cidade){
+    if(!cidade){
+        alert('Sem cidade inserida.')
         return;
     }
 
-    const timeURL = `https://api.ipgeolocation.io/timezone?apiKey=${timeKEY}&lat=${latitude}&long=${longitude}`
+    const timeURL = `https://api.ipgeolocation.io/timezone?apiKey=${timeKEY}&location=${cidade}`
 
-    fetch(timeURL)
-        .then(responseTime => responseTime.json())
-        .then(dataTime => {
-            console.log(dataTime)    
-        })
-        .catch(error => {
+    try{ 
+        const response = await fetch(timeURL)
+        const data = await response.json()
+        console.log(data)
+        const dataCompletaCidade = data.date_time_txt
+        const dataConvertida = converterData(dataCompletaCidade)
+        return dataConvertida
+        }
+    catch(error) {
             console.error('Erro fetching current date:', error);
             alert('Error fetching current date. Please try again.')
-        })
-
-
-    // const dataCidadeFormatada = `${dia} ${mes} ${hora}:${minuto}`
-    // return dataCidadeFormatada
+        }
 }
 
-function displayWeather(data){
-    console.log(data)
-
-    const cityName = data.name
-    const cityCountry = data.sys.country
-    const cityLong = data.coord.lon
-    const cityLat = data.coord.lat
-
-    const dataCidadeFormatada = timeLocal(cityLong, cityLat)
-    // inserirInfosCidade(cityName, cityCountry, dataCidadeFormatada)
-
-
+async function displayWeather(cidade,pais,date){
+    await inserirInfosCidade(cidade, pais, date)
+    
 
     // const climaDescricao = data.weather[0].description
     // const climaIcone = data.weather[0].icon
@@ -89,16 +84,15 @@ function displayWeather(data){
     // const cityWindDirection = data.wind.deg
     // const citySunriseUnix = data.sys.sunrise
     // const citySunsetUnix = data.sys.sunset
-
-    // var cityNascerDoSol = timeConverter(citySunriseUnix)
-    // var cityPorDoSol = timeConverter(citySunsetUnix)
 }
 
-function inserirInfosCidade(cidade,pais,data){
-    cityInfo.innerHTML = `
-        <h1 class="city-name">${cidade}, ${pais}</h1>
-        <div class="city-date-container">
-            <h2 class="city-date">${data}</h2>
+async function inserirInfosCidade(cidade,pais,data){
+    cityInfos.innerHTML = `
+        <div class="city-info">
+            <h1 class="city-name">${cidade}, ${pais}</h1>
+            <div class="city-date-container">
+                <h2 class="city-date">${data}</h2>
+            </div>
         </div>
     `
 }
